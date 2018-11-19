89390e8f388612d9c24787b019c168cf5cbe873a
forkpoons <forkpoons@gmail.com>
2018-11-19 11:34:13 +0700

    pc vervion

--- js/index.js
+++ js/index.js
@@ -42,57 +42,13 @@ input.onchange = function () {
         const file = document.getElementById('uploaded-file1').files[0];
         if (file) {
             if (document.getElementById('uploaded-file1').files.length === 1) {
-                if (file.name.length < 30)
+                if (file.name.length < 28)
                     document.getElementById('texts').innerHTML = '' + file.name;
                 else
-                    document.getElementById('texts').innerHTML = file.name.slice(0, 5) + ' ... ' + file.name.slice(-20);
+                    document.getElementById('texts').innerHTML = file.name.slice(0, 5) + ' ... ' + file.name.slice(-18);
             }
             else
                 document.getElementById('texts').innerHTML = 'Выбранно изображений: ' + document.getElementById('uploaded-file1').files.length;
         }
     }
 };
-
-$('#demo').daterangepicker({
-    "singleDatePicker": true,
-    "timePicker": true,
-    "timePicker24Hour": true,
-    "timePickerIncrement": 20,
-    "locale": {
-        "format": "DD/MM/YYYY HH:mm",
-        "separator": " - ",
-        "applyLabel": "Ок",
-        "cancelLabel": "Отмена",
-        "fromLabel": "From",
-        "toLabel": "To",
-        "customRangeLabel": "Custom",
-        "daysOfWeek": [
-            "Вс",
-            "Пн",
-            "Вт",
-            "Ср",
-            "Чт",
-            "Пт",
-            "Сб"
-        ],
-        "monthNames": [
-            "Январь",
-            "Февраль",
-            "Март",
-            "Апрель",
-            "Май",
-            "Июнь",
-            "Июль",
-            "Август",
-            "Сентябрь",
-            "Октябрь",
-            "Ноябрь",
-            "Декабрь"
-        ],
-        "firstDay": 1
-    },
-    "startDate": "12/11/2018 09:00",
-    "endDate": "12/11/2018"
-}, function(start, end, label) {
-    console.log("New date range selected: '" + start.format('YYYY-MM-DD-HH-mm') + "' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')");
-});
